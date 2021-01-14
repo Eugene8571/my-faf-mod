47d1fb709ca79a9f09bf7d3f063c0c75bff5afd6
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-01-14 19:31:01 +0200

    Create mod_info.lua

---
+++ mod_info.lua
@@ -0,0 +1,10 @@
+name = "My FAF mod"
+version = 0.1
+copyright = "WTFPL"
+description = "control engi"
+author = "Death"
+url = ""
+uid = "13213235-2351-6183-sfs6-653518715422"
+exclusive = false
+ui_only = true
+conflicts = {}
