7dabe30b8ee6b83ea8948f7c20a92b670497348f
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-01-17 03:41:16 +0200

    Update smart_engi.lua

--- modules/smart_engi.lua
+++ modules/smart_engi.lua
@@ -1,7 +1,11 @@
 
 
-function smart_engi()
-    local unit = GetIdleEngineers()[1]
+local function move_random()
+    local selection = GetSelectedUnits()
+    local unit = selection[1]
+
+    if not unit then return print('-') end
+
     local avgPoint = {0,0}
     avgPoint[1] = avgPoint[1] + unit:GetPosition()[1]
     avgPoint[2] = avgPoint[2] + unit:GetPosition()[3]
@@ -10,23 +14,29 @@ function smart_engi()
     local r = math.random( 0, 360 )
     target_position[1] = target_position[1] + 10*math.sin(r)
     target_position[3] = target_position[3] + 10*math.cos(r)
+
     avgPoint[1] = target_position[1] - avgPoint[1]
     avgPoint[2] = target_position[3] - avgPoint[2]
-
     local rotation = math.atan(avgPoint[1]/avgPoint[2])
     rotation = rotation * 180 / math.pi
     if avgPoint[2] < 0 then
         rotation = rotation + 180
     end
+
     local cb = {Func="AttackMove", 
                 Args={
                     Target=target_position, 
                     Rotation = rotation, 
                     Clear=false}}
 
-    local tblUnits = {unit}
-    local position = target_position
-    -- IssueAggressiveMove(tblUnits, position)
     SimCallback(cb, true)
-    print( '...' )
+    local commandQueue = unit:GetCommandQueue()
+    print(table.getn(commandQueue))
+end
+
+
+function smart_engi()
+    for i=1,6 do
+        move_random()
+    end
 end
