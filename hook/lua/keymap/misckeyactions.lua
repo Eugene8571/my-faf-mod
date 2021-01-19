d06087b31437a40509a9dd5e6ba8aea1ebc0b610
Eugene8571 <40310109+Eugene8571@users.noreply.github.com>
2021-01-19 05:33:03 +0200

    unfinished

--- hook/lua/keymap/misckeyactions.lua
+++ hook/lua/keymap/misckeyactions.lua
@@ -1,13 +1,24 @@
 local KeyMapper = import('/lua/keymap/keymapper.lua')
 
-function say_hi(S)
-    print(S)
-end
+smartEngi = import("/mods/my-faf-mod/modules/smart_engi.lua")
 
-smart_engi = import("/mods/my-faf-mod/modules/smart_engi.lua")
 --------HOTKEYS-----------
 
---Default
-KeyMapper.SetUserKeyAction('Castom', {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').say_hi('F')", category = 'M y op', order = 75})
-KeyMapper.SetUserKeyAction('smart_engi', {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.smart_engi()", category = 'M y op', order = 85})
+KeyMapper.SetUserKeyAction('test', 
+    {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.testMove()", category = 'Smart Engi', order = 85})
+
+KeyMapper.SetUserKeyAction('Move to mass deposits', 
+    {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.to_mass_deposits()", category = 'Smart Engi', order = 85})
+
+KeyMapper.SetUserKeyAction('Build MEX', 
+    {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.build_mex()", category = 'Smart Engi', order = 85})
+
+KeyMapper.SetUserKeyAction('make AttackMove random', 
+    {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.attack_move_random()", category = 'Smart Engi', order = 85})
+
+KeyMapper.SetUserKeyAction('make Move random', 
+    {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.plan_move_random()", category = 'Smart Engi', order = 85})
+
+KeyMapper.SetUserKeyAction('All custom moves in Chain', 
+    {action = "UI_Lua import('/lua/keymap/misckeyactions.lua').smart_engi.chain_moves()", category = 'Smart Engi', order = 85})
 
